94d90cdf60e870083f5bca9e3acd5e8747349917
mciolkosz <mciolkosz@bspfsystems.org>
2021-01-26 14:45:28 -0600

    Updates to fit BSPF System, LLC's style.

--- src/main/java/org/bspfsystems/example/ExampleMod.java
+++ src/main/java/org/bspfsystems/example/ExampleMod.java
@@ -14,13 +14,17 @@
 // limitations under the License.                                             //
 ////////////////////////////////////////////////////////////////////////////////
 
-pluginManagement {
-    repositories {
-        jcenter()
-        maven {
-            name = 'Fabric'
-            url = 'https://maven.fabricmc.net/'
-        }
-        gradlePluginPortal()
-    }
+package org.bspfsystems.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+	}
 }
--- src/main/java/org/bspfsystems/example/mixin/ExampleMixin.java
+++ src/main/java/org/bspfsystems/example/mixin/ExampleMixin.java
@@ -14,13 +14,18 @@
 // limitations under the License.                                             //
 ////////////////////////////////////////////////////////////////////////////////
 
-pluginManagement {
-    repositories {
-        jcenter()
-        maven {
-            name = 'Fabric'
-            url = 'https://maven.fabricmc.net/'
-        }
-        gradlePluginPortal()
-    }
+package org.bspfsystems.example.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		System.out.println("This line is printed by an example mod mixin!");
+	}
 }
